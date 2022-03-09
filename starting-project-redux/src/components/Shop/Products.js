56f60891018cac4b50d0ef441351148c71d0bec4
Nikola Pantic <Nikola.Pantic@rs.ey.com>
2022-03-09 16:54:43 +0100

    added redux first project

---
+++ starting-project-redux/src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const Products = (props) => {
+  const DUMMY_PRODUCTS = [
+    {
+      id: "p1",
+      price: 6,
+      title: "My first book",
+      description: "My first book I have ever wrote.",
+    },
+    {
+      id: "p2",
+      price: 4,
+      title: "My second book",
+      description: "My second book I have ever wrote.",
+    },
+  ];
+
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            title={product.title}
+            price={product.price}
+            description={product.description}
+            key={product.id}
+            id={product.id}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
